08cee04531b7f242e9e715f598c83cca03f5674f
Marc S. Brooks <marc@mbrooks.info>
2013-03-24 14:25:35 -0700

    Updated copyright notice

--- jquery.webform.toolkit.js
+++ jquery.webform.toolkit.js
@@ -22,7 +22,7 @@
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 		}
 		else
-		if (typeof method === 'object' || ! method) {
+		if (typeof method === 'object' || !method) {
 			return methods.init.apply(this, arguments);
 		}
 		else {
