532fec0e24e01d0de39e9f023b2b6e80b1aea1ad
Marc S. Brooks <marc@mbrooks.info>
2024-02-24 10:09:02 -0800

    Removed config, used create() method

--- test/e2e/input-hidden.js
+++ test/e2e/input-hidden.js
@@ -3,17 +3,17 @@
 import {browser, expect, $} from '@wdio/globals';
 
 describe('Input hidden element', function() {
-  let fieldset;
+  let form;
 
   beforeEach(async function() {
     await browser.url(`${process.cwd()}/demo/index.html`);
 
-    fieldset = await $('.field-group0');
+    form = await $('.webform');
   });
 
   describe('Field', function() {
     it('should contain attributes', async function() {
-      const field = await fieldset.$('input[type="hidden"]');
+      const field = await form.$('input[type="hidden"]');
 
       await expect(field).toHaveAttribute('type', 'hidden', {
         message: 'Attribute type="hidden" is defined'
